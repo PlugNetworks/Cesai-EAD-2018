d57fcf387e5bd544f0a6fef5c60a5f7679ff254d
PlugNetworks <alexandro@plugnetworks.com.br>
2017-12-26 20:16:15 -0200

    Controller de Método Atualizado para criação da View.

--- app/Http/Controllers/MetodoController.php
+++ app/Http/Controllers/MetodoController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+Use DB;
+Use App\CategoriaModel;
+Use App\CursoInfo;
 use App\Http\Requests;
 
 class MetodoController extends Controller
 {
-    //
+    public function index()
+    {
+        $CategoriaModels        = CategoriaModel::orderBy('id')->get();
+        $CursoInfos              = CursoInfo::orderBy('id')->get();
+
+        return view('metodo.index', [
+
+        ], compact('CategoriaModels', 'CursoInfos'));
+    }
 }
